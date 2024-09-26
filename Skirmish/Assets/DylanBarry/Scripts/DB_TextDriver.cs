cbb394e197b78bb69e67d4667ffca07a59d469d8
STUDENT Dylan Barry <Dylan.Barry@students.ittralee.ie>
2024-09-26 12:55:51 +0100

    Signed-off-by: STUDENT Dylan Barry <Dylan.Barry@students.ittralee.ie>

---
+++ Skirmish/Assets/DylanBarry/Scripts/DB_TextDriver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DB_TextManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform TextCloneTemplate;
+    void Start()
+    {
+        Transform mytextGO = Instantiate(TextCloneTemplate);
+        DB_TextInstanceScript myText = mytextGO.GetComponent<DB_TextInstanceScript>();
+        myText.initialize("Hello");
+        myText.SetText("Hello");
+     //   myText.AttachTo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
