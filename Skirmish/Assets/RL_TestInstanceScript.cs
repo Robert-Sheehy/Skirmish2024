8f34930a3876d51dbd2cff59a2b23a68ef61dedd
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2024-10-02 17:08:35 +0100

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

--- Skirmish/Assets/RL_TestInstanceScript.cs
+++ Skirmish/Assets/RL_TestInstanceScript.cs
@@ -2,35 +2,52 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RL_TestInstanceScript : MonoBehaviour
 {
+    float timer = 0;
+    bool isFlashing = false;
+    float flashFrequency;
     TMPro.TextMeshPro m_TextMeshPro;
 
     internal void AttachTo(Transform transformToAttachTo)
     {
-       transform.parent = transformToAttachTo;
+        m_TextMeshPro.alignment = TMPro.TextAlignmentOptions.Center;
+        m_TextMeshPro.rectTransform.pivot = new Vector2(0.5f, 0);
+
+        transform.parent = transformToAttachTo;
     }
 
     internal void initialize(string newText)
     {
         m_TextMeshPro = GetComponent<TMPro.TextMeshPro>();
+
+ 
         m_TextMeshPro.text = newText;
     }
 
     internal void SetColor(Color newColour)
     {
-        throw new NotImplementedException();
+        m_TextMeshPro.color = newColour;
     }
 
     internal void SetPosition(Vector2 vector2)
     {
-        throw new NotImplementedException();
+     
     }
 
     internal void SetText(string newText)
     {
         m_TextMeshPro.text = newText;
+    }
+
+    internal void StartFlash(float frequencyOfFlash)
+    {
+        flashFrequency = frequencyOfFlash;
+        m_TextMeshPro.alpha = 1.0f;
+        timer = 0;
+        isFlashing = true;
     }
 
     // Start is called before the first frame update
@@ -42,8 +59,16 @@ public class RL_TestInstanceScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   if (isFlashing)
+        {
+            timer += Time.deltaTime;
+
+
+            m_TextMeshPro.alpha = Mathf.Pow(Mathf.Cos(timer * flashFrequency * Mathf.PI),2) ;
+
+        }
+
        
-        transform.LookAt(Camera.main.transform.position);
+        transform.LookAt(2* transform.position- Camera.main.transform.position);
     }
 }
