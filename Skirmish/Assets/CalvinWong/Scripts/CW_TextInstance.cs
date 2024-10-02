07d2619c37192d5253477912534a70a183114df9
STUDENT Calvin C. Wong <Calvin.C.Wong@students.ittralee.ie>
2024-10-02 17:08:12 +0100

    Flashing text in Unity Signed-off-by: STUDENT Calvin C. Wong <Calvin.C.Wong@students.ittralee.ie>

--- Skirmish/Assets/CalvinWong/Scripts/CW_TextInstance.cs
+++ Skirmish/Assets/CalvinWong/Scripts/CW_TextInstance.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class CW_TextInstance : MonoBehaviour
 {
 
     TMPro.TextMeshPro m_TextMeshPro;
+    float timer = 0;
+    bool isFlashing = false;
 
     internal void AttachTo(Transform transformToAttachTo)
     {
+
+        m_TextMeshPro.alignment = TMPro.TextAlignmentOptions.Center;
+        //m_TextMeshPro.pivot = (0.5,0);
+
+        //m_TextMeshPro = GetComponent<TMPro.TextMeshPro>();
+        //m_TextMeshPro.rectTransform.anchoredPosition = Vector3.zero;
         transform.parent = transformToAttachTo;
     }
 
@@ -33,6 +42,36 @@ public class CW_TextInstance : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isFlashing)
+        {
+            timer += Time.deltaTime;
+
+            m_TextMeshPro.alpha = Mathf.Pow(Mathf.Cos(timer * Mathf.PI),2);
+        }
+
+        transform.LookAt(-Camera.main.transform.position);
+    }
+
+    internal void SetColor(Color green)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void SetPosition(Vector2 vector2)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void StartFlash(float frequencyOfFlash)
+    {
+        m_TextMeshPro.alpha = 1.0f;
+        timer = 0;
+        isFlashing = true;
+    }
+
+    internal static CW_TextInstance GetText()
+    {
+        throw new NotImplementedException();
     }
 }
