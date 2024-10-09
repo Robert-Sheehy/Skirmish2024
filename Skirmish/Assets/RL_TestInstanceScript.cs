e0b677581c46509a23263d6b3fbfcb100ee58f6e
LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>
2024-10-09 14:00:19 +0100

    Signed-off-by: LECTURER Robert Sheehy <lt00036625@365s.ittralee.ie>

--- Skirmish/Assets/RL_TestInstanceScript.cs
+++ Skirmish/Assets/RL_TestInstanceScript.cs
@@ -10,13 +10,20 @@ public class RL_TestInstanceScript : MonoBehaviour
     bool isFlashing = false;
     float flashFrequency;
     TMPro.TextMeshPro m_TextMeshPro;
+    private bool isOnObject;
 
     internal void AttachTo(Transform transformToAttachTo)
     {
+        transform.SetParent( transformToAttachTo, false);
         m_TextMeshPro.alignment = TMPro.TextAlignmentOptions.Center;
+        m_TextMeshPro.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        m_TextMeshPro.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
         m_TextMeshPro.rectTransform.pivot = new Vector2(0.5f, 0);
+        
+        m_TextMeshPro.rectTransform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
 
-        transform.parent = transformToAttachTo;
+        m_TextMeshPro.fontSize = 18;
+        isOnObject = true;
     }
 
     internal void initialize(string newText)
@@ -34,7 +41,7 @@ public class RL_TestInstanceScript : MonoBehaviour
 
     internal void SetPosition(Vector2 vector2)
     {
-     
+     m_TextMeshPro.rectTransform.SetPositionAndRotation(vector2, Quaternion.identity);
     }
 
     internal void SetText(string newText)
@@ -68,7 +75,7 @@ public class RL_TestInstanceScript : MonoBehaviour
 
         }
 
-       
+       if (isOnObject)
         transform.LookAt(2* transform.position- Camera.main.transform.position);
     }
 }
