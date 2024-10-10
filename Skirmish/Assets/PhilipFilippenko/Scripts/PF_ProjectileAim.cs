24e1f178b642807bb98136571245ebd4a35555f4
STUDENT Philip Filippenko <Philip.Filippenko@students.ittralee.ie>
2024-10-10 13:01:48 +0100

    Signed-off-by: STUDENT Philip Filippenko <Philip.Filippenko@students.ittralee.ie>

--- Skirmish/Assets/PhilipFilippenko/Scripts/PF_ProjectileAim.cs
+++ Skirmish/Assets/PhilipFilippenko/Scripts/PF_ProjectileAim.cs
@@ -1,21 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PF_ProjectileAim : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject targetPlane;
+    Renderer myRenderer;
+    float singleTargetMaxRange = 10;
+    float areaTargetMaxRange = 20;
+    float defaultScale = 0.1f;
+    float tinyLift = 0.01f;
+
     void Start()
     {
-        
+        targetPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        targetPlane.GetComponent<Collider>(). = false;
+        targetPlane.transform.position = Vector3.zero;
+        targetPlane.transform.rotation = Quaternion.identity;
+        targetPlane.transform.localScale = defaultScale*Vector3.one;
+        myRenderer = targetPlane.GetComponent<Renderer>();
+        myRenderer.material.color = Color.red;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(ray.origin, ray.direction*50);
-            if (Physics.Raycast(ray))
-                print("Hit");
+            RaycastHit info;
+            if (Physics.Raycast(ray, out info))
+                print("Hit" + info.transform.gameObject.name);
+                targetPlane.transform.up = info.normal;
+                targetPlane.transform.position = info.point + tinyLift*info.normal;
+                targetPlane.transform.localScale = defaultScale * Vector3.one;
+
+                float distanceFromArcherToPoint = Vector3.Distance(theArcher.transform.position info.point);
+                if (distanceFromArcherToPoint < singleTargetMaxRange)
+                    myRenderer.material.color = Color.green;
+                else
+                    if (distanceFromArcherToPoint < areaTargetMaxRange)
+                        myRenderer.material.color = Color.Lerp(Color.red,Color.yellow, 0.5f);
+                        else
+                { 
+                    myRenderer.material.color = Color.red;
+                }
     }
 }
