f85c85b6ee729a18c08ab6e7f9a6cd4408df200d
STUDENT Rani Wheeza <Rani.Wheeza@students.ittralee.ie>
2024-12-05 12:55:01 +0000

    Signed-off-by: STUDENT Rani Wheeza <Rani.Wheeza@students.ittralee.ie>

--- Skirmish/Assets/RaniW/RW_Final/RW_Sheild.cs
+++ Skirmish/Assets/RaniW/RW_Final/RW_Sheild.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class RW_Sheild : RW_Item
 {
+    private float protectValue;
+    private float dexteritiyValue;
+
+    public RW_Sheild(string newItemName, string newItemDescription, float newItemWeight, float newItemProtectValue, float newItemDexterityValue)
+    {
+        name = newItemName;
+        description = newItemDescription;
+        weight = newItemWeight;
+        protectValue = newItemProtectValue;
+        dexteritiyValue = newItemDexterityValue;
+    }
+
     internal void Block()
     {
         Debug.Log("Am blocking you");
